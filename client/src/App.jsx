239baf2b519d142c01119178c206b0d11143aee2
Manthan-Gohil <manthangohil22577@gmail.com>
2025-02-07 12:45:06 +0530

    Setup frontend and backend

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='h-screen w-full flex items-center justify-center bg-zinc-900 text-white text-[2rem]'>
+      <h1>CourseDocify</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ server/app.js
@@ -0,0 +1,27 @@
+import express from "express"
+import dotenv from "dotenv"
+import connectDB from "./database/db.js"
+import cors from "cors"
+
+const app = express()
+
+dotenv.config()
+
+connectDB();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}))
+
+const PORT = process.env.PORT || 3000
+
+app.get('/', (req, res) => {
+    res.send('Server has started successfully !!!');
+});
+
+app.listen(PORT,()=>{
+    console.log(`Server is listening to port ${PORT}`);
+})
